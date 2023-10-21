aeb24262f53f2ba358b6fc4dac683aa24634d602
Michael Hewitt <michael@hewitts.us>
2023-10-21 09:04:29 -0700

    basic menu bar

--- Task Tracker/Task_TrackerApp.swift
+++ Task Tracker/Task_TrackerApp.swift
@@ -5,13 +5,31 @@
 //  Created by Michael Hewitt (Home) on 10/21/23.
 //
 
+// from https://sarunw.com/posts/swiftui-menu-bar-app/
+
 import SwiftUI
 
 @main
 struct Task_TrackerApp: App {
+    // 1
+    @State var currentNumber: String = "1"
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+        }
+        // 2
+        MenuBarExtra(currentNumber, systemImage: "\(currentNumber).circle") {
+            // 3
+            Button("One") {
+                currentNumber = "1"
+            }
+            Button("Two") {
+                currentNumber = "2"
+            }
+            Button("Three") {
+                currentNumber = "3"
+            }
         }
     }
 }
