5632841a5f91b619ae1b3514d271f71f88f75a58
Michael Hewitt <michael@hewitts.us>
2023-10-21 08:54:21 -0700

    Initial Commit

---
+++ Task Tracker/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Task Tracker
+//
+//  Created by Michael Hewitt (Home) on 10/21/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Task Tracker/Task_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrackerApp.swift
+//  Task Tracker
+//
+//  Created by Michael Hewitt (Home) on 10/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
